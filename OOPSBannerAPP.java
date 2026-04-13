682be335f8fb529b20dd3ab8f1d05ccc141b0fae
Sanskar <sanskarofficial30@gmail.com>
2026-04-13 14:44:20 +0530

    Coded to  Print OOPS to Console

---
+++ OOPSBannerAPP.java
@@ -0,0 +1,45 @@
+class OOPSBannerApp {
+    public static void main(String[] args) {
+        //UC1
+        System.out.println("  OOPS ");
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        //UC2
+        System.out.println(" *** " + "   *** " + "  ****** " + "  ***** ");
+        System.out.println("** **" + "  ** **" + "  **   **" + "  **   **");
+        System.out.println("** **" + "  ** **" + "  **   **" + "  **     ");
+        System.out.println("** **" + "  ** **" + "  ******" + "   ***** ");
+        System.out.println("** **" + "  ** **" + "  **" + "             **");
+        System.out.println("** **" + "  ** **" + "  **" + "        **   **");
+        System.out.println(" *** " + "   *** " + "  **" + "         ***** ");
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        //UC3
+        System.out.println(String.join("  "," *** " , "    *** " , "  ****** " , "  ***** "));
+        System.out.println(String.join("  ","** **" , "   ** **" , "  **   **"  ,"  **   **"));
+        System.out.println(String.join("  ","** **" , "   ** **" , "  **   **" , " **     "));
+        System.out.println(String.join("  ","** **" , "   ** **" , "  ******" , "   ***** "));
+        System.out.println(String.join("  ","** **" , "   ** **" , "  **" , "           **"));
+        System.out.println(String.join("  ","** **" , "   ** **" , "  **" , "       **   **"));
+        System.out.println(String.join("  "," *** " , "    *** " , "  **" , "        ***** "));
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        //UC4
+        
+    }
+}
